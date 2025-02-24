898a4799b27faede05a5986362c6632e0b409904
Kiana Ren <xingzhi.ren@mail.mcgill.ca>
2025-02-24 01:09:24 +0100

    connect database to front-end

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -10,15 +10,15 @@ function MovieCard({movie}){
     return <div className="movie-card">
         <Link to={`/dashboard/${movie.id}`} className="movie-link">
         <div className="movie-poster">
-            <img src = {movie.url} alt = {movie.title}/>
+            <img src = {movie.posterURL} alt = {movie.title}/>
             <div className="movie-overlay">
                 <button className="list-btn" onClick={onListBtnClick}>ADD TO LIST</button>
             </div>
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.genre}</p>
-            <p>{movie.year}</p>
+            <p>{movie.releaseYear}</p>
+            <p>{movie.criticScore}</p>
         </div>
         </Link>
     </div>
