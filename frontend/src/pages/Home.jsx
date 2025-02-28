22a25cbe02ab4988244ae06243684506dc1a4197
Kiana Ren <xingzhi.ren@mail.mcgill.ca>
2025-02-28 21:28:54 +0000

    complete searching function

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,12 +7,31 @@ function Home(){
     const [searchQuery, setSearchQuery] = useState("");
     const [startYear, setStartYear] = useState("");
     const [endYear, setEndYear] = useState("");
+    const [minRuntime, setMinRuntime] = useState("");
+    const [maxRuntime, setMaxRuntime] = useState("");
+    const [allTags, setAllTags] = useState([]); 
+    const [selectedTags, setSelectedTags] = useState([]); 
+    const [tagLimitWarning, setTagLimitWarning] = useState("");
     const [selectedGenres, setSelectedGenres] = useState([]); 
     const [allGenres, setAllGenres] = useState([]);
     const [movies, setMovies] = useState([]);
     const [page, setPage] = useState(1);
     const [totalMovies, setTotalMovies] = useState(0);
     const limit = 50;  // 50 movies per page
+
+    // Fetch available genres on mount
+    useEffect(() => {
+        axios.get("/api/genres")
+            .then((response) => setAllGenres(response.data.genres))
+            .catch((error) => console.error("Error fetching genres: ", error));
+    }, []);
+
+    useEffect(() => {
+        axios.get("/api/tags")
+            .then(response => setAllTags(response.data))
+            .catch(error => console.error("Error fetching tags:", error));
+    }, []);
+    
 
     const fetchMovies = () => {
         const queryParams = new URLSearchParams({
@@ -23,6 +42,14 @@ function Home(){
 
         if (startYear) queryParams.append("startYear", startYear);
         if (endYear) queryParams.append("endYear", endYear);
+        if (minRuntime) queryParams.append("minRuntime", minRuntime);
+        if (maxRuntime) queryParams.append("maxRuntime", maxRuntime);
+        if (selectedGenres.length > 0) queryParams.append("genres", selectedGenres.join(","));
+        if (selectedTags.length > 0) {
+            queryParams.append("tags", selectedTags.join(","));
+        }
+
+
 
         axios.get(`/api/movies?${queryParams.toString()}`)
             .then((response) => {
@@ -42,6 +69,45 @@ function Home(){
         fetchMovies();
     };
 
+    const handleClear = () => {
+        setSearchQuery("");
+        setStartYear("");
+        setEndYear("");
+        setMinRuntime("");
+        setMaxRuntime("");
+        setSelectedGenres([]);
+        setSelectedTags([]);
+        setPage(1);
+        fetchMovies(); // Fetch all movies again
+    };
+
+    const toggleGenre = (genre) => {
+        setSelectedGenres(prev =>
+            prev.includes(genre) ? prev.filter(g => g !== genre) : [...prev, genre]
+        );
+    };
+
+    const toggleTagSelection = (selectedTag) => {
+        setTagLimitWarning(""); 
+
+        setSelectedTags((prevSelected) => {
+            if (prevSelected.length >= 10) {  
+                setTagLimitWarning("Can only pick up to 10 tags"); 
+                return prevSelected; // Do not add new tag
+            }
+            if (!prevSelected.includes(selectedTag)) {
+                return [...prevSelected, selectedTag];  // Add if not selected
+            }
+            return prevSelected; 
+        });
+    };
+    
+    const removeTag = (tagToRemove) => {
+        setSelectedTags((prevSelected) => prevSelected.filter(tag => tag !== tagToRemove));
+        setTagLimitWarning(""); 
+    };
+    
+
     return (
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
@@ -54,9 +120,11 @@ function Home(){
                         onChange={(e) => setSearchQuery(e.target.value)}
                     />
                     <button type="submit" className="search-button">Search</button>
+                    <button type="button" className="clear-button" onClick={handleClear}>Clear</button>
+
                 </div>
                 <div className="year-filter">
-                    <span>From</span>
+                    <span>Release Year: </span>
                     <input 
                         type="number" 
                         placeholder="Start Year" 
@@ -64,7 +132,7 @@ function Home(){
                         value={startYear}
                         onChange={(e) => setStartYear(e.target.value)}
                     />
-                    <span>to</span>
+                    <span>~</span>
                     <input 
                         type="number" 
                         placeholder="End Year" 
@@ -73,6 +141,60 @@ function Home(){
                         onChange={(e) => setEndYear(e.target.value)}
                     />
                 </div>
+                <div className="year-filter">
+                    <span>Runtime(minutes): </span>
+                    <input 
+                        type="number" 
+                        placeholder="Min " 
+                        className="year-input"
+                        value={minRuntime}
+                        onChange={(e) => setMinRuntime(e.target.value)}
+                    />
+                    <span>~</span>
+                    <input 
+                        type="number" 
+                        placeholder="Max " 
+                        className="year-input"
+                        value={maxRuntime}
+                        onChange={(e) => setMaxRuntime(e.target.value)}
+                    />
+                </div>
+                <div className="genres-filter">
+                    {allGenres.map((genre) => (
+                        <span
+                            key={genre}
+                            className={`genre-item ${selectedGenres.includes(genre) ? "selected" : ""}`}
+                            onClick={() => toggleGenre(genre)}
+                        >
+                            {genre}
+                        </span>
+                    ))}
+                </div>
+                <div className="tag-filter">
+                    <span>Tags: (pick up to 10)</span>
+                    <select 
+                        multiple 
+                        className="tag-dropdown"
+                        size="5"
+                        onChange={(e) => toggleTagSelection(e.target.value)}
+                    >
+                        {allTags.map(tag => (
+                            <option key={tag.tagId} value={tag.tag}>{tag.tag}</option>
+                        ))}
+                    </select>
+
+                    {/* Display Selected Tags with "X" Button */}
+                    <div className="selected-tags">
+                        {selectedTags.map(tag => (
+                            <span key={tag} className="selected-tag">
+                                {tag} 
+                                <button className="remove-tag" onClick={() => removeTag(tag)}>✖</button>
+                            </span>
+                        ))}
+                    </div>
+                </div>
+
+
 
                 
             </form>
