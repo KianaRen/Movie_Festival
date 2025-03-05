c225550c48215f68c018159fdd76ba01b9098c49
Kiana Ren <xingzhi.ren@mail.mcgill.ca>
2025-03-05 13:38:33 +0000

    finish add to list funtion

--- frontend/src/components/MovieListCard.jsx
+++ frontend/src/components/MovieListCard.jsx
@@ -1,25 +1,31 @@
 import { Link } from 'react-router-dom';
 import '../css/MovieCard.css';
-import axios from "axios";
 
 function MovieListCard({ movie, onRemove }) {
-
     return (
         <div className="movie-card">
+            {/* Remove button (X) in the top-right corner */}
+            <button 
+                className="remove-button"
+                onClick={(e) => { 
+                    e.preventDefault(); // Prevent navigation when clicking "X"
+                    onRemove(); 
+                }}
+            >
+                ✖
+            </button>
+
             <Link to={`/dashboard/${movie.movieId}`} className="movie-link">
                 <div className="movie-poster">
                     <img src={movie.posterURL} alt={movie.title} />
-                    <div className="movie-overlay">
-                    
-                    </div>
-                </div>
-                <div className="movie-info">
-                    <h3>{movie.title}</h3>
-                    <p>{movie.releaseYear}</p>
-                    <p>{movie.criticScore}</p>
-                    <button className="remove-button" onClick={onRemove}>Remove</button>
                 </div>
             </Link>
+
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.releaseYear}</p>
+                <p>{movie.criticScore}</p>
+            </div>
         </div>
     );
 }
